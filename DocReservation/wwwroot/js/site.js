7a060817e86cd0c89eb286d6ec4af884b5448701
Mostafa Said <m.ssaid356@gmail.com>
2025-08-11 03:08:21 +0300

    Add files via upload

---
+++ DocReservation/wwwroot/js/site.js
@@ -0,0 +1,24 @@
+﻿// Simple mobile menu toggle
+document.getElementById('mobile-menu-button').addEventListener('click', () => {
+    const menu = document.getElementById('mobile-menu');
+    menu.classList.toggle('hidden');
+});
+
+
+// for Complete Appointment
+document.addEventListener('DOMContentLoaded', function () {
+    const appointmentForm = document.getElementById('appointmentForm');
+    const successModal = document.getElementById('successModal');
+    const closeModal = document.getElementById('closeModal');
+
+    appointmentForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+        successModal.classList.remove('hidden');
+        successModal.classList.add('flex');
+    });
+
+    closeModal.addEventListener('click', function () {
+        successModal.classList.add('hidden');
+        successModal.classList.remove('flex');
+    });
+});
